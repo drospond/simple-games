05b128214bd389e26a8a0bc859ba92336a226d3f
drospond <david.rospond@gmail.com>
2020-07-13 23:32:11 -0400

    added and styled title

---
+++ client/src/Containers/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Home.scss";
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row title-row">
+                    <h1 id="title">Simple Games</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
