17802a5501ec067e05700d653e199690c86f775a
drospond <david.rospond@gmail.com>
2020-07-14 22:28:51 -0400

    react router set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import "./App.css";
 import Home from "./Containers/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+import SignIn from "./Containers/SignIn/SignIn";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Home/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
