08336823d2bef9ea6aa717aca6bd236935ab2ac0
drospond <david.rospond@gmail.com>
2020-07-14 22:59:40 -0400

    sign in form set up with minimal styling

--- client/src/Containers/SignIn/SignIn.jsx
+++ client/src/Containers/SignIn/SignIn.jsx
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./SignIn.scss";
 
 class SignIn extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Sign In</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="row title-row">
+          <h1 className="title">Sign In</h1>
+        </div>
+        <form id="signin-form">
+          <div className="form-group">
+            <label for="userNameInput">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              id="userNameInput"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label for="passwordInput">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="passwordInput"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Sign in
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
