6ae794c803b5b0e47a1f9cf92d7246274a1c7d89
drospond <david.rospond@gmail.com>
2020-07-26 14:08:57 -0400

    display room code

--- client/src/Containers/TicTacToe/TicTacToe.jsx
+++ client/src/Containers/TicTacToe/TicTacToe.jsx
@@ -3,9 +3,16 @@ import GameChat from "../../Components/GameChat/GameChat";
 import { joinRoom } from "../../Redux/actions";
 import { connect } from "react-redux";
 import socket from "../../socket.io";
+import './TicTacToe.scss';
+
+function mapStateToProps(state) {
+    const { roomCode } = state
+    return { roomCode: roomCode }
+  }
 
 class TicTacToe extends Component {
   componentDidMount() {
+    console.log(this);
     const room = sessionStorage.getItem("room");
     if (room) {
       this.props.joinRoom(room);
@@ -21,7 +28,7 @@ class TicTacToe extends Component {
           <h1 className="title">Tic-Tac-Toe</h1>
         </div>
         <div className="row">
-          {/* <h4>{display room code}</h4> */}
+          <h4 id="room-code">Room: {this.props.roomCode}</h4>
         </div>
         <div className="row">
           <GameChat socket={socket} />
@@ -31,4 +38,4 @@ class TicTacToe extends Component {
   }
 }
 
-export default connect(null, { joinRoom })(TicTacToe);
+export default connect(mapStateToProps, { joinRoom })(TicTacToe);
