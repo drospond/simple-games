124c1d3f1645e9bfab40f8bbbd78e7da02934500
David Rospond <david.rospond@gmail.com>
2020-12-08 15:58:02 -0500

    Wrong letters render stick figure piece

--- client/src/Containers/HangMan/HangMan.jsx
+++ client/src/Containers/HangMan/HangMan.jsx
@@ -18,7 +18,8 @@ function mapStateToProps(state) {
 class HangMan extends Component {
   state = {
     word: [],
-    guess: [],
+    guesses: [],
+    letterGuess: "",
     letterBank: [],
     wrongGuesses: 0,
   };
@@ -50,27 +51,74 @@ class HangMan extends Component {
     // });
   }
 
-  handleChange = (event) => {
+  handleChangeWord = (event) => {
     const { name, value } = event.target;
     this.setState({
-      [name]: value.toUpperCase().split(''),
-      error: false
+      [name]: value.toUpperCase().split(""),
+      error: false,
+    });
+  };
+
+  handleChangeLetter = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value.toUpperCase(),
+      error: false,
     });
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    if(/^(\w ?[,'-]?){4,64}$/.test(this.state.word.join(''))){
-        document.getElementById('hang-man-form').className = "hidden";
-        console.log("SUBMIT!!");
-    }else{
-        this.setState({
-            error: "Invalid entry. Word/phrase can't contain special characters other than a comma, appostrophe, or dash and must be less that 64 characters."
-        })
+    if (/^(\w ?[,'-]?){4,64}$/.test(this.state.word.join(""))) {
+      document.getElementById("hang-man-form").className = "no-display";
+      document
+        .getElementById("game-start-wrapper")
+        .classList.remove("no-display");
+    } else {
+      this.setState({
+        error:
+          "Invalid entry. Word/phrase can't contain special characters other than a comma, appostrophe, or dash and must be less that 64 characters.",
+      });
     }
   };
 
+  handleGuess = (event) => {
+    event.preventDefault();
+    if (!/[a-zA-z]?/.test(this.state.letterGuess)) {
+      this.setState({
+        error: "Must guess a letter!",
+      });
+      return;
+    }
+    if (this.state.guesses.includes(this.state.letterGuess)) {
+      return this.setState({
+        error: "Letter already guessed!",
+      });
+    }
+    if (!this.state.word.includes(this.state.letterGuess)) {
+        this.setState({
+          wrongGuesses: this.state.wrongGuesses + 1,
+        });
+    }
+    this.setState({
+      guesses: this.state.guesses.concat(this.state.letterGuess),
+    });
+  };
+
+  hangManPieces = [
+    <div className="hang-head"></div>,
+    <div className="hang-piece hang-body"></div>,
+    <div className="hang-piece hang-arm-l"></div>,
+    <div className="hang-piece hang-arm-r"></div>,
+    <div className="hang-piece hang-leg-l"></div>,
+    <div className="hang-piece hang-leg-r"></div>,
+  ];
+
   render() {
+      const renderedHangManPieces = []
+      for(let i = 0; i < this.state.wrongGuesses; i++){
+            renderedHangManPieces.push(this.hangManPieces[i]);
+      }
     return (
       <div className="container">
         <div className="row title-row">
@@ -80,33 +128,97 @@ class HangMan extends Component {
           <h4 id="room-code">Room: {this.props.roomCode}</h4>
         </div>
         <div className="row">
-          <div id="hang-man-board">
+          <div id="hang-man-board" className="col">
             {this.props.playerNumber == 1 && (
-              <form onSubmit={e=>this.handleSubmit(e)} id="hang-man-form">
-                <h4 id="hang-man-question">Choose a word or phrase</h4>
+              <form onSubmit={(e) => this.handleSubmit(e)} id="hang-man-form">
+                <h4 className="hang-man-question">Choose a word or phrase</h4>
                 <input
                   type="text"
                   className="form-control"
                   id="hangManWordInput"
                   name="word"
-                  onChange={this.handleChange}
+                  autoComplete="off"
+                  onChange={this.handleChangeWord}
                 />
                 <button type="submit" id="submit-word" class="btn btn-primary">
                   Submit
                 </button>
               </form>
             )}
-            <div className="hang-piece hang-base"></div>
-            <div className="hang-piece hang-staff"></div>
-            <div className="hang-piece hang-reach"></div>
-            <div className="hang-piece hang-rope"></div>
-            <div className="hang-head"></div>
-            <div className="hang-piece hang-body"></div>
-            <div className="hang-piece hang-arm-l"></div>
-            <div className="hang-piece hang-arm-r"></div>
-            <div className="hang-piece hang-leg-l"></div>
-            <div className="hang-piece hang-leg-r"></div>
-            <div className="hang-piece letter-line"></div>
+            <div id="game-start-wrapper" className="no-display">
+              <div className="row">
+                <div className="col">
+                  <div id="man-section">
+                    <div className="hang-piece hang-base"></div>
+                    <div className="hang-piece hang-staff"></div>
+                    <div className="hang-piece hang-reach"></div>
+                    <div className="hang-piece hang-rope"></div>
+                    {renderedHangManPieces}
+                  </div>
+                </div>
+                <div className="col-8">
+                  <div className="guessed-letter-section">
+                    {this.state.guesses.map((letter) => {
+                      if (!this.state.word.includes(letter)) {
+                        return letter;
+                      }
+                    })}
+                  </div>
+                  <div id="word-section">
+                    {this.state.word.map((letter) => {
+                      let letterClass = "";
+                      if (/[ ]/.test(letter)) {
+                        letter = "___";
+                        letterClass = "hidden";
+                      }
+                      if (
+                        /[A-Z]/.test(letter) &&
+                        !this.state.guesses.includes(letter)
+                      ) {
+                        letterClass = "hidden";
+                      }
+                      if (/[_+'-]/.test(letter)) {
+                        return (
+                          <div className="hang-letter">
+                            <div className={letterClass}>{letter}</div>
+                          </div>
+                        );
+                      }
+                      return (
+                        <div className="hang-letter">
+                          <div className={letterClass}>{letter}</div>
+                          <div className="hang-piece letter-line"></div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div id="guess-section">
+                <form
+                  id="guess-form"
+                  onSubmit={(event) => this.handleGuess(event)}
+                >
+                  <h4 className="hang-man-question">Guess a Letter</h4>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="hangManLetterInput"
+                    name="letterGuess"
+                    maxLength="1"
+                    autoComplete="off"
+                    onChange={this.handleChangeLetter}
+                  />
+                  <button
+                    type="submit"
+                    id="submit-letter"
+                    class="btn btn-primary"
+                  >
+                    Guess
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
         <div className="row">
