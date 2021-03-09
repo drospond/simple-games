6ebf7347a8f788558ef43ac1ffb2ee9603bb9edf
drospond <david.rospond@gmail.com>
2021-03-09 11:55:25 -0500

    cleaned up old console.logs

--- client/src/socket.io/index.js
+++ client/src/socket.io/index.js
@@ -5,9 +5,7 @@ const ENDPOINT = "/";
 
 const socket = io(ENDPOINT);
 socket.initializeListeners = function(dispatch){
-    console.log("index level dispatch: ", dispatch);
     initializeRoomListeners(this, dispatch);
-    console.log("this is what this is: ", this);
 }
 
 export default socket;
